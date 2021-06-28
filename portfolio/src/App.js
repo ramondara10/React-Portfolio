f5812cfa27451667844312b76f7d63aedcaf463c
ramondara10 <68656067+ramondara10@users.noreply.github.com>
2021-06-27 19:54:18 -0500

    forgot to do initial commit worked on files and added other folders

---
+++ portfolio/src/App.js
@@ -0,0 +1,43 @@
+import './index.css';
+
+import React, { useState } from 'react';
+
+import About from './components/About';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Resume from './components/Resume'
+import Projects from './components/Projects';
+import { sections } from './utils/navItems';
+
+function App() {
+  const [currentSection, setCurrentSection] = useState('About');
+
+  const renderPage = () => {
+    switch (currentSection) {
+      case 'Projects':
+        return <Projects />;
+      case 'Contact':
+        return <Contact />;
+      case 'Resume':
+        return <Resume />;
+      default:
+        return <About />;
+    }
+  };
+  return (
+    <div className='app-container'>
+      <div className='content-wrap'>
+        <Header
+          sections={sections}
+          currentSection={currentSection}
+          setCurrentSection={setCurrentSection}
+        />
+      </div>
+      <Footer />
+    </div>
+
+  );
+}
+
+export default App;
---
+++ portfolio/src/utils/footerLinks.js
@@ -0,0 +1,20 @@
+export const links =[
+    {
+		id: 'link-1',
+		name: 'LinkedIn',
+		url: 'https://www.linkedin.com/in/ramon-alvarado-027aa1215/',
+		icon: 'linkedin',
+	},
+	{
+		id: 'link-2',
+		name: 'GitHub',
+		url: 'https://github.com/ramondara10',
+		icon: 'github-square',
+	},
+	{
+		id: 'link-3',
+		name: 'Stack Overflow',
+		url: 'https://stackoverflow.com/users/14834108/ramon-a-alvarado-rivera',
+		icon: 'stack-overflow',
+	},
+];
